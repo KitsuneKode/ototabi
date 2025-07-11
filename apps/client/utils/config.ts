29d18cee7660a176492ba316acce895a7b707a6f
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2025-07-12 00:10:07 +0530

    fixing issues random

--- apps/client/utils/config.ts
+++ apps/client/utils/config.ts
@@ -3,21 +3,15 @@ import { ConfigLoader } from '@ototabi/common/config-loader'
 const clientConfigSchema = {
   frontendUrl: () => process.env.NEXT_PUBLIC_APP_URL || '',
   apiBaseUrl: () => process.env.NEXT_PUBLIC_API_URL || '',
-// Removed jwtSecret from clientConfigSchema as it should not be exposed on the client side.
   databaseUrl: () => process.env.DATABASE_URL || '',
   nodeEnv: () => process.env.NODE_ENV || 'development',
 }
 
 const config = ConfigLoader.getInstance(clientConfigSchema, 'client')
 
-config.validate([
-  'frontendUrl',
-  'apiBaseUrl',
-  'jwtSecret',
-  'databaseUrl',
-  'nodeEnv',
-])
+config.validate(['frontendUrl', 'apiBaseUrl', 'nodeEnv'])
 
-console.log('Configuration loaded:', config.getConfig('apiBaseUrl'))
-
+if (config.getConfig('nodeEnv') === 'development') {
+  console.log('Configuration loaded:', config.getConfig('apiBaseUrl'))
+}
 export default config
